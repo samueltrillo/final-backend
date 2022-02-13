1392f688c69af0524ac22a951f1fb432c0c2a8e5
samueltrillo <knubbe10@gmail.com>
2022-02-13 17:47:08 -0300

    deleted commented lines

--- index.js
+++ index.js
@@ -16,25 +16,6 @@ app.use(cors());
 
 app.use(router);
 
-// ANTE CUALQUIER PROBLEMA AL CONECTAR A LA BASE DE DATOS, REVISAR
-// 1.- Que la URI tenga correctamente setedo el nombre, la contraseña y la base de datos a la que le pega
-// 2.- Verificar que ese usuario y contraseña este añadido en DATABASE ACCESS
-// 3.- Verificar de que esté permitida la IP del usuario (si no, setear de que cualquier IP pueda conectarse)
-
-// mongoose
-//   .connect(MONGO_URI)
-//   .then(() => {
-//     console.log("Conectado a la base de datos");
-
-//     app.listen({ port: PORT }, () => {
-//       console.log(`Servidor corriendo en el puerto ${PORT}`);
-//     });
-//   })
-//   .catch((error) => {
-//     console.log(`Error: ${error}`);
-//     console.log("No fue posible conectarse a la base de datos");
-//   });
-
 const connectDb = async () => {
   try {
     await mongoose.connect(MONGO_URI);
